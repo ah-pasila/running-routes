95d93169f0c5e267d431d0774b2d0dc3839bdb0c
Ahpasila <aura.pasila@gmail.com>
2022-12-02 23:39:52 +0200

    Add database column definitions

--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT,
-    password TEXT
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE routes (
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     type TEXT,
     length NUMERIC(5,1),
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    created_by INTEGER REFERENCES users
 );
 
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
-    route_id INTEGER REFERENCES routes
-    grade INTEGER,
-    review TEXT
+    route_id INTEGER REFERENCES routes,
+    grade INTEGER NOT NULL,
+    review TEXT NOT NULL
 );
-
